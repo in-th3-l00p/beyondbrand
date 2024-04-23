98f4d573930ea4c497d098ab816ca5da3ae4df68
in-th3-l00p <indianucode0303@gmail.com>
2024-04-23 22:48:36 +0300

    unfinished generation

---
+++ frontend/src/app/api/brands/generateLogo/route.ts
@@ -0,0 +1,40 @@
+import {z} from "zod";
+import openai from "@/utils/openai";
+import {NextResponse} from "next/server";
+
+const BodySchema = z.object({
+    name: z.string().max(255),
+    description: z.string().max(255),
+    color: z.array(z.string()).max(6),
+});
+
+export async function POST(req: Request) {
+    let jsonBody;
+    try {
+        jsonBody = await req.json();
+    } catch (e) {
+        return new Response(
+            JSON.stringify({errors: "Invalid JSON jsonBody."}),
+            {status: 400}
+        );
+    }
+
+    const body = BodySchema.safeParse(jsonBody);
+    if (!body.success) {
+        return new Response(
+            JSON.stringify({errors: body.error}),
+            {status: 400}
+        );
+    }
+
+    const response = await openai.images.generate({
+        model: "dall-e-3",
+        quality: "hd",
+        style: "natural",
+        n: 1,
+        response_format: "b64_json",
+        prompt: `A logo for a brand named ${body.data.name} with the following description: ${body.data.description}, and the following colors: ${body.data.color.join(", ")}.`,
+    });
+
+    return NextResponse.json(response.data);
+}
--- frontend/src/app/brands/create/steps/LogoGeneration.tsx
+++ frontend/src/app/brands/create/steps/LogoGeneration.tsx
@@ -5,6 +5,24 @@ export default function LogoGeneration() {
     return (
         <div className="form-container">
             <FormLabel back>Generate a logo</FormLabel>
+
+            <div className={
+                "w-[300px] h-[300px] rounded-md border-2 border-cyan " +
+                "flex flex-col gap-2 justify-center items-center text-center " +
+                "text-zinc-400 mb-4"
+            }>
+                <h2 className={"text-lg"}>Here is the preview region</h2>
+                <p className={"font-light"}>
+                    Press on the {`"Generate"`} button to get your first logo
+                </p>
+            </div>
+
+            <button
+                type={"button"}
+                className={"btn"}
+            >
+                Generate
+            </button>
         </div>
     );
 }

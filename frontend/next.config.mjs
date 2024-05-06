fe54c2bce64e28b8d3fdb4ae2eb30e33cb288d62
in-th3-l00p <indianucode0303@gmail.com>
2024-05-06 20:53:22 +0300

    added s3 images to next js

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        remotePatterns: [
+            {
+                protocol: "https",
+                hostname: "beyondbrand-storage.s3.eu-north-1.amazonaws.com",
+                port: "",
+                pathname: "/**"
+            }
+        ]
+    }
+};
 
 export default nextConfig;

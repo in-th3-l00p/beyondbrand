b3a5a41639f572096c569a5e621b7a3e5904a50d
in-th3-l00p <indianucode0303@gmail.com>
2024-05-27 09:50:47 +0300

    nginx proxy grtt pow

--- generation-service/src/main.ts
+++ generation-service/src/main.ts
@@ -5,6 +5,10 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
+app.get("/api/generation", (req, res) => {
+    res.send({ generation: "generation" });
+})
+
 app.listen(PORT, () => {
     winston.info("Server started on port " + PORT);
 });

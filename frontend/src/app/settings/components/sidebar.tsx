ddb6a690477d853a8292cf8b25bd3f823683ae0d
nayrow <paseresebastian0504@gmail.com>
2024-05-31 21:18:34 +0300

    settings directory with layout

---
+++ frontend/src/app/settings/components/sidebar.tsx
@@ -0,0 +1,122 @@
+'use client'
+import React from "react";
+import Image from "next/image";
+import * as Icon from "react-feather";
+import Link from "next/link";
+import {usePathname} from "next/navigation";
+import {useSession} from "next-auth/react";
+import Loading from "@/app/brands/create/components/Loading";
+import User from "@/app/interfaces/User";
+
+
+
+export default function Sidebar() {
+    const {data: session, status, update} = useSession();
+
+    if (status === "loading" || !session)
+        return <Loading/>
+    const path = usePathname();
+
+    const user = session.user as User;
+
+    return (
+        path === "/settings" ?
+            <nav className={"flex flex-col gap-2 w-screen items-center"}>
+                <div id={"minimal-account-info"} className={"flex gap-4 items-center p-2"}>
+                    {user.image &&
+                        <Image src={user.image} alt={"profile"} width={50} height={50}
+                               className={"rounded-full"}/>}
+                    {!user.image && <Icon.User className={"h-max"}/>}
+                    <div>
+                        <h1 className={"font-bold"}>{user.name}</h1>
+                        <p>{user.email}</p>
+                    </div>
+                </div>
+                <div
+                    className={path.includes("account") ? "flex gap-4 p-2 rounded-md bg-light-gray" : "flex gap-4 p-2"}>
+                    <Icon.User/>
+                    <Link href={"/settings/account"}>
+                        Your Account
+                    </Link>
+                </div>
+                <div
+                    className={path.includes("business") ? "flex gap-4 p-2 rounded-md bg-light-gray" : "flex gap-4 p-2"}>
+                    <Icon.Briefcase/>
+                    <Link href={"/settings/business"}>
+                        Your Business
+                    </Link>
+                </div>
+                <div
+                    className={path.includes("security") ? "flex gap-4 p-2 rounded-md bg-light-gray" : "flex gap-4 p-2"}>
+                    <Icon.Lock/>
+                    <Link href={"/settings/security"}>
+                        Login & Security
+                    </Link>
+                </div>
+                <div
+                    className={path.includes("notifications") ? "flex gap-4 p-2 rounded-md bg-light-gray" : "flex gap-4 p-2"}>
+                    <Icon.Mail/>
+                    <Link href={"/settings/notifications"}>
+                        Notifications Preferences
+                    </Link>
+                </div>
+                <div
+                    className={path.includes("privacy") ? "flex gap-4 p-2 rounded-md bg-light-gray" : "flex gap-4 p-2"}>
+                    <Icon.Lock/>
+                    <Link href={"/settings/privacy"}>
+                        Privacy Settings
+                    </Link>
+                </div>
+            </nav> :
+            <>
+                <nav className={"md:flex flex-col gap-2 w-full hidden pl-4 pt-4"}>
+                    <div id={"minimal-account-info"} className={"flex w-max gap-4 items-center p-2"}>
+                        {user.image &&
+                            <Image src={user.image} alt={"profile"} width={50} height={50}
+                                   className={"rounded-full"}/>}
+                        {!user.image && <Icon.User className={"h-max"}/>}
+                        <div>
+                            <h1 className={"font-bold"}>{user.name}</h1>
+                            <p>{user.email}</p>
+                        </div>
+                    </div>
+                    <div
+                        className={path.includes("account") ? "flex gap-4 p-2 rounded-md bg-light-gray" : "flex gap-4 p-2"}>
+                        <Icon.User/>
+                        <Link href={"/settings/account"}>
+                            Your Account
+                        </Link>
+                    </div>
+                    <div
+                        className={path.includes("business") ? "flex gap-4 p-2 rounded-md bg-light-gray" : "flex gap-4 p-2"}>
+                        <Icon.Briefcase/>
+                        <Link href={"/settings/business"}>
+                            Your Business
+                        </Link>
+                    </div>
+                    <div
+                        className={path.includes("security") ? "flex gap-4 p-2 rounded-md bg-light-gray" : "flex gap-4 p-2"}>
+                        <Icon.Lock/>
+                        <Link href={"/settings/security"}>
+                            Login & Security
+                        </Link>
+                    </div>
+                    <div
+                        className={path.includes("notifications") ? "flex gap-4 p-2 rounded-md bg-light-gray" : "flex gap-4 p-2"}>
+                        <Icon.Mail/>
+                        <Link href={"/settings/notifications"}>
+                            Notifications Preferences
+                        </Link>
+                    </div>
+                    <div
+                        className={path.includes("privacy") ? "flex gap-4 p-2 rounded-md bg-light-gray" : "flex gap-4 p-2"}>
+                        <Icon.Lock/>
+                        <Link href={"/settings/privacy"}>
+                            Privacy Settings
+                        </Link>
+                    </div>
+                </nav>
+            </>
+
+    )
+}
---
+++ frontend/src/app/settings/layout.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Sidebar from "@/app/settings/components/sidebar";
+
+export default function SettingLayout({children,}: Readonly<{ children: React.ReactNode; }>) {
+    return (
+        <div className={"flex w-screen bg-"}>
+            <div className={"md:w-max"}>
+                <Sidebar/>
+            </div>
+            <div className={"w-screen overflow-x-hidden"}>
+                {children}
+            </div>
+        </div>
+    );
+}

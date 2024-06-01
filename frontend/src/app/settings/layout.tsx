cd6c0de5599007619ef105cedddff90d7cc02926
nayrow <paseresebastian0504@gmail.com>
2024-06-01 15:29:20 +0300

    typo error in layout

--- frontend/src/app/settings/layout.tsx
+++ frontend/src/app/settings/layout.tsx
@@ -3,7 +3,7 @@ import Sidebar from "@/app/settings/components/sidebar";
 
 export default function SettingLayout({children,}: Readonly<{ children: React.ReactNode; }>) {
     return (
-        <div className={"flex w-screen bg-"}>
+        <div className={"flex w-screen"}>
             <div className={"md:w-max"}>
                 <Sidebar/>
             </div>

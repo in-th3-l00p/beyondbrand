0e802f15d1affd4ae4c5ccec750677e2b3d35703
nayrow <paseresebastian0504@gmail.com>
2024-06-01 11:33:45 +0300

    added user and business interface

---
+++ frontend/src/app/interfaces/Business.tsx
@@ -0,0 +1,27 @@
+interface Metric {
+    year: number;
+    revenue: number;
+    profit: number;
+    debt: number;
+    long_term_assets: number;
+    short_term_assets: number;
+    equity: number;
+    employees: number;
+}
+
+interface Email {
+    address: string;
+    usage: string;
+}
+
+export default interface Business {
+    id: number;
+    name: string;
+    emails: Email[];
+    logo: string;
+    worldwide: boolean;
+    countries?: string[];
+    phones: string[];
+    website: string;
+    metrics: Metric[];
+}
--- frontend/src/app/interfaces/User.tsx
+++ frontend/src/app/interfaces/User.tsx
@@ -1,6 +1,9 @@
+import Business from "@/app/interfaces/Business";
+
 export default interface User {
         id: number;
         name: string;
         email: string;
         image: string;
+        business: Business
 }

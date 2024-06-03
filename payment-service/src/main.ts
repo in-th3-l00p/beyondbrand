d85482a1177333d85f16ec41de0ad09c6a28a638
in-th3-l00p <indianucode0303@gmail.com>
2024-06-03 11:38:44 +0300

    updated nginx config

---
+++ payment-service/src/main.ts
@@ -0,0 +1,32 @@
+import express, {json} from "express";
+import logger from "./utils/logger";
+import morgan from "morgan";
+import cors from "cors";
+import initializeAmqp from "./utils/amqp";
+import initializeMongoose from "./utils/mongoose";
+
+require("dotenv").config();
+
+const app = express();
+
+app.use(cors({
+    origin: "*"
+}));
+app.use(morgan("combined", {
+    "stream": {
+        write: (message) => {
+            logger.info(message);
+        }
+    }
+}));
+app.use(json());
+
+(async () => {
+    await initializeMongoose();
+    await initializeAmqp();
+
+    const PORT = process.env.PORT || 3002;
+    app.listen(() => {
+        logger.info("Server started on port " + PORT + ".");
+    })
+})();

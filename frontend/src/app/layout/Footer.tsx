f307f1ad41a66622895b664375653ca1839b00a3
nayrow <paseresebastian0504@gmail.com>
2024-06-03 16:18:11 +0300

    fixed paddings and bugs on footer

--- frontend/src/app/layout/Footer.tsx
+++ frontend/src/app/layout/Footer.tsx
@@ -4,12 +4,12 @@ import Link from "next/link";
 
 export default function Footer() {
     return (
-        <footer className={"w-full px-16 py-8 bg-dark-gray text-ghost-white"}>
+        <footer className={"w-full px-8 md:px-16 py-8 bg-dark-gray text-ghost-white"}>
             <div className={"w-full flex pb-4"}>
-                <div className={"w-full gap-16 grid grid-cols-2 lg:grid-cols-4 "}>
+                <div className={"w-full flex flex-col md:flex-row space-y-8 md:space-y-0 md:space-x-8 md:gap-0"}>
                     <div>
                         <h2 className={"text-4xl font-bold pb-4"}>Services</h2>
-                        <ul className={"list-disc space-y-2"}>
+                        <ul className={"space-y-2"}>
                             <li><Link href={"/brands/create"}>Name generation</Link></li>
                             <li><Link href={"/brands/create"}>Description generation</Link></li>
                             <li><Link href={"/brands/create"}>Color scheme generation</Link></li>
@@ -20,21 +20,21 @@ export default function Footer() {
                     </div>
                     <div>
                         <h2 className={"text-4xl font-bold pb-4"}>Networking</h2>
-                        <ul className={"list-disc space-y-2"}>
+                        <ul className={"space-y-2"}>
                             <li><Link href={"/blog/posts"}>Blog</Link></li>
                             <li><Link href={"/forum/posts"}>Forum</Link></li>
                         </ul>
                     </div>
                     <div>
                         <h2 className={"text-4xl font-bold pb-4"}>Social</h2>
-                        <ul className={"list-disc space-y-2"}>
+                        <ul className={"space-y-2"}>
                             <li><Link href={"/"}>Tiktok</Link></li>
                             <li><Link href={"/"}>Instagram</Link></li>
                         </ul>
                     </div>
                     <div>
                         <h2 className={"text-4xl font-bold pb-4"}>Company</h2>
-                        <ul className={"list-disc space-y-2"}>
+                        <ul className={"space-y-2"}>
                             <li><Link href={"/about"}>About</Link></li>
                             <li><Link href={"/contact"}>Contact</Link></li>
                         </ul>

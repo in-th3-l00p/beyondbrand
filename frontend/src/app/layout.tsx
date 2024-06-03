563730377668df9c6bca5cfe687510b1e5554afc
nayrow <paseresebastian0504@gmail.com>
2024-06-03 16:19:37 +0300

    commit for merge

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -7,6 +7,8 @@ import "@/database/mongoose";
 import NextAuthSessionProvider from "@/app/layout/NextAuthSessionProvider";
 import {Toaster} from "react-hot-toast";
 import Footer from "@/app/layout/Footer";
+import Amqp from "streaming";
+import logger from "@/utils/logger";
 
 const inter = Tilt_Neon({subsets: ["latin"]});
 
@@ -15,21 +17,23 @@ export const metadata: Metadata = {
     description: "Branding generator webapp",
 };
 
-export default function RootLayout({
-    children,
-}: Readonly<{
+export default async function RootLayout({
+                                             children,
+                                         }: Readonly<{
     children: React.ReactNode;
 }>) {
+    if (!Amqp.isInitialized())
+        await Amqp.initializeFromEnv(logger);
     return (
         <html lang="en">
-            <body className={inter.className + " min-h-screen overflow-x-hidden flex flex-col"}>
-                <NextAuthSessionProvider>
-                    <Header/>
-                    <Toaster/>
-                    {children}
-                    <Footer/>
-                </NextAuthSessionProvider>
-            </body>
+        <body className={inter.className + " min-h-screen overflow-x-hidden flex flex-col"}>
+        <NextAuthSessionProvider>
+            <Header/>
+            <Toaster/>
+            {children}
+            <Footer/>
+        </NextAuthSessionProvider>
+        </body>
         </html>
     );
 }

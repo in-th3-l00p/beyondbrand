953816c59c419006416cd20cbfc7465a55ec5c27
nayrow <paseresebastian0504@gmail.com>
2024-06-03 16:12:03 +0300

    modified container

--- frontend/src/app/settings/components/Container.tsx
+++ frontend/src/app/settings/components/Container.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Container({children,}: Readonly<{ children: React.ReactNode; }>) {
     return (
-        <div className={"p-6 overflow-x-hidden lg:mr-64 xl:mr-72 2xl:mr-80"}>
+        <div className={"pt-6 px-6 overflow-x-hidden lg:mr-64 xl:mr-72 2xl:mr-80"}>
             {children}
         </div>
     )

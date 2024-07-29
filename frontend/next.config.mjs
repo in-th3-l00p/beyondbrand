04fcbea1bab9fa35003d06baa9ff08d6f1292269
in-th3-l00p <indianucode0303@gmail.com>
2024-07-29 21:49:40 +0300

    added strapi image support

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -20,7 +20,13 @@ const nextConfig = {
                 hostname: "lh3.googleusercontent.com",
                 port: "",
                 pathname: "/**"
-            }
+            },
+            {
+                protocol: "https",
+                hostname: "cms.beyondbrand.pro",
+                port: "",
+                pathname: "/**"
+            },
         ]
     }
 };
